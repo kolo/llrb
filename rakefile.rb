8012910bab4b3efdb408c4f404699e6b62c99333
Dmitry Maksimov <dmtmax@gmail.com>
2017-04-17 11:05:55 +0200

    Setup rubocop

---
+++ rakefile.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+require 'rake/testtask'
+require 'rubocop/rake_task'
+
+Rake::TestTask.new do |t|
+  t.libs << 'test'
+  t.test_files = FileList['test/**/test_*.rb']
+end
+
+RuboCop::RakeTask.new(:rubocop) do |t|
+  t.options = %w(-c ./.rubocop.yml)
+end
+
+task default: %w(test)
